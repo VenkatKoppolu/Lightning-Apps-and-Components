abe8ba11e3e8ed4a4e3a8a349f8c8002fc8ff02c
VenkatKoppolu <vkoppolu@talentrover.com>
2018-04-02 16:12:32 +0530

    initial commit

--- src/aura/expenseItem/expenseItemController.js
+++ src/aura/expenseItem/expenseItemController.js
@@ -7,5 +7,11 @@
             component.set("v.formatdate", new Date(mydate));
         }
 
-	}
+	},clickReimbursed: function(component, event, helper) {
+        var expense = component.get("v.expense");
+        var updateEvent = component.getEvent("updateExpense");
+        updateEvent.setParams({ "expense": expense });
+        updateEvent.fire();
+    }
+
 })
